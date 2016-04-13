855bbb0cd0b28e1e8ced1215284e2821ab560e33
lucasbrito007 <lucas_eduardo_brito@hotmail.com>
2016-04-13 19:55:46 -0300

    Create PlanetaDAO.java
    Classe PlanetaDAO, implementa o método getPlaneta  para busca de um objeto Planeta dentro do BD

---
+++ PlanetaDAO.java
@@ -0,0 +1,32 @@
+
+package etech;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PlanetaDAO {
+    
+    public Planeta getPlaneta (String nomePlaneta) throws SQLException{
+        String sql = "SELECT * FROM Planeta WHERE nome = ?";
+        Connection conexao = DriverManager.getConnection(null);
+        PreparedStatement estato = conexao.prepareStatement(sql);
+        estato.setString(1, nomePlaneta);
+        ResultSet resultado = estato.executeQuery();
+        Planeta novo = null;
+        if(resultado.next()){
+            int[] coordenadas = new int[3];
+            coordenadas[0] = resultado.getInt("x");
+            coordenadas[1] = resultado.getInt("y");
+            coordenadas[2] = resultado.getInt("z");
+            novo = new Planeta(resultado.getString("nome"), resultado.getString("tipo"), coordenadas);
+        }
+        resultado.close();
+        estato.close();
+        conexao.close();
+        return novo;
+    }
+    
+}
